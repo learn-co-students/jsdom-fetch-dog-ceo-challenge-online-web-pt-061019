a92dd49bfc292e94da32deed912c13f06e803967
Brittany Javalera <brittjavalera@gmail.com>
2020-01-15 12:44:30 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+function fetchImages() {
+    fetch(imgURL)
+      .then(resp => resp.json())
+      .then(json => renderImages(json))
+  }
+  
+  function renderImages(json) {
+    json.forEach( imgURL => {
+      const imageContainer = document.getElementById('dog-image-container')
+      const img = document.createElement('img')
+      img.src = `${imgURL}`
+      imageContainer.appendChild(img)
+    })
+  }
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    fetchImages()
+  })
+  
