cd941fd93054023aabec0f1519a6cae676b131df
Brittany Javalera <brittjavalera@gmail.com>
2020-01-15 20:50:10 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -2,40 +2,51 @@ console.log('%c HI', 'color: firebrick')
 
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
 const breedUrl = 'https://dog.ceo/api/breeds/list/all'
-
-function fetchImages() {
-    fetch(imgUrl)
-      .then(resp => resp.json())
-      .then(json => renderImages(json))
-  };
-  
-  function fetchBreeds() {
-    fetch(breedUrl)
-      .then(resp => resp.json())
-      .then(json => renderBreeds(json))
-  };
-
-  function renderImages(json) {
-    json.message.forEach( image => {
-      const imageContainer = document.getElementById('dog-image-container')
-      const img = document.createElement('img')
-      img.src = image
-      imageContainer.appendChild(img)
-    });
-  };
-
-  function renderBreeds(json) {
-      Object.keys(json.message).forEach( breed => {
-      const dogBreedList = document.getElementById('dog-breeds')
-      const listItem = document.createElement('li')
-      listItem.innerText = breed
-      dogBreedList.appendChild(listItem)
-    });
-  };
   
   document.addEventListener('DOMContentLoaded', function() {
+    
+    function fetchImages() {
+        fetch(imgUrl)
+          .then(resp => resp.json())
+          .then(json => renderImages(json))
+      };
+      
+      function fetchBreeds() {
+        fetch(breedUrl)
+          .then(resp => resp.json())
+          .then(json => renderBreeds(json))
+      };
+    
+      function renderImages(json) {
+        json.message.forEach( image => {
+          const imageContainer = document.getElementById('dog-image-container')
+          const img = document.createElement('img')
+          img.src = image
+          imageContainer.appendChild(img)
+        });
+      };
+    
+      function renderBreeds(json) {
+          Object.keys(json.message).forEach( breed => {
+          const dogBreedUl = document.getElementById('dog-breeds')
+          const breedLi = document.createElement('li')
+          breedLi.innerText = breed
+          dogBreedUl.appendChild(breedLi)
+         });
+        };
+
     fetchImages()
     fetchBreeds()
-    
+
+    const dogBreedUl = document.getElementById('dog-breeds')
+    dogBreedUl.addEventListener('click', function(event){
+        event.target.style.color = 'green'
+    });
+
+    const alphaDropdown = document.getElementById('.breed-dropdown')
+    alphaDropdown.addEventListener('change', function(event) {
+        const letter = event.target.value
+        
+    })
   })
   
