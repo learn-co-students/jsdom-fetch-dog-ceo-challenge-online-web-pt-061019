0d5c448e4cc5173748d4fba9f96eecf3a969a53d
Mario Adamo Jr <mario.adamo.jr@gmail.com>
2020-01-16 20:08:09 -0500

    challenge 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener("DOMContentLoaded", () => {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    fetch(imgUrl)
+    .then(response => {
+        return response.json()
+    })
+    .then(dogImages => { 
+        dogImages.message.forEach(image => {
+            addImage(image)
+        });
+        
+    })
+})
+
+function addImage(imageUrl){
+    const newImage = document.createElement('img')
+    newImage.src = imageUrl
+    const imageCollection = document.getElementById('dog-image-container')
+    imageCollection.appendChild(newImage)
+}
