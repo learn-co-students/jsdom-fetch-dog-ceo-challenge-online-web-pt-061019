9403b01a83609dc3f8c5079b33b953b9731d12a2
scrabill <shannon.crabill@gmail.com>
2020-01-16 19:36:32 -0500

    Challenge 2

--- src/index.js
+++ src/index.js
@@ -1,30 +1,120 @@
-console.log('%c HI', 'color: firebrick')
+console.log('%c Woof', 'color: firebrick')
 
-document.addEventListener("DOMContentLoaded", function(){
-  const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+const breedUrl = "https://dog.ceo/api/breeds/list/all";
+
+let breeds = []
+
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("Start");
+  fetchImgs();
+  fetchBreeds();
+});
+
+function fetchImgs() {
   return fetch(imgUrl)
     .then(resp => resp.json())
     .then(json => printImg(json))
-});
-
-function printImg(json) {
-  console.log("Woof");
-  console.log(json);
-  const dogImageContainer = document.getElementById("dog-image-container");
-
-  for (i = 0; i < json.length;i++) {
-    console.log(json[i]);
-  }
-
-  // json.forEach(x => {
-  //   const img = document.createElement('img')
-  //   img.src = `x`
-  //   dogImageContainer.appendChild(img);
-  // })
 }
 
+function fetchBreeds(){
+  return fetch(breedUrl)
+    .then(resp => resp.json())
+    .then(json => createBreedsArray(json))
+};
+
+function createBreedsArray(json) {
+  allBreeds = Object.keys(json.message);
+  renderBreeds(allBreeds);
+}
+
+
+
+function renderBreeds(breeds) {
+  console.log("renderBreeds. Woof!!");
+
+  const dogBreedsContainer = document.getElementById("dog-breeds");
+
+  breeds.forEach(breed => {
+    let li = document.createElement("li");
+    li.textContent = breed;
+    dogBreedsContainer.appendChild(li);
+  })
+};
+
+
+function printImg(json) {
+  console.log("Woof!!");
+  // console.log(json);
+  // console.log(json.message.length)
+
+  let dogImage = json.message
+
+  const dogImageContainer = document.getElementById("dog-image-container");
+
+  for (image of dogImage) {
+    const img = document.createElement("img")
+    img.src = image;
+    dogImageContainer.appendChild(img);
+  }};
+
 //
+// let breeds = [];
+// const dogBreedsContainer = document.getElementById("dog-breeds");
 //
-// return fetch("https://anapioficeandfire.com/api/books")
-//   .then(resp => resp.json())
-//   .then(json => renderBooks(json))
+// document.addEventListener("DOMContentLoaded", function(){
+//   const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+//   return fetch(imgUrl)
+//     .then(resp => resp.json())
+//     .then(json => printImg(json))
+// });
+//
+// function printImg(json) {
+//   console.log("Woof");
+//   // console.log(json);
+//   // console.log(json.message.length)
+//
+//   let dogImage = json.message
+//
+//   const dogImageContainer = document.getElementById("dog-image-container");
+//
+//   for (image of dogImage) {
+//     const img = document.createElement("img")
+//     img.src = image;
+//     dogImageContainer.appendChild(img);
+//   }
+//
+// // #2
+//
+// function fetchBreeds(){
+//   const breedUrl = "https://dog.ceo/api/breeds/list/all";
+//   return fetch(breedUrl)
+//     .then(resp => resp.json())
+//     .then(json => createBreedsArray(json))
+// };
+//
+// function createBreedsArray(json) {
+//   allBreeds = Object.keys(json.message);
+//   renderBreeds(allBreeds);
+// }
+//
+// function renderBreeds(json) {
+//   console.log("Woof!!");
+//
+//   breeds.forEach(breed => {
+//     let li = document.createElement("li");
+//     li.textContent = breed;
+//     dogBreedsContainer.appendChild(li);
+//
+//   })
+//
+//   //
+//   // for (breed of breeds) {
+//   //   const li = document.createElement("li");
+//   //   li.textContent = breed;
+//   //   dogBreedsContainer.appendChild(li);
+//   // }
+//
+// }
+//
+// }
