388a48249ba5e8debc388a15346797d8f3bc88df
scrabill <shannon.crabill@gmail.com>
2020-01-16 21:16:00 -0500

    Challenge #4

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ document.addEventListener("DOMContentLoaded", function() {
   console.log("Start");
   fetchImgs();
   fetchBreeds();
+  sortBreeds();
 });
 
 function fetchImgs() {
@@ -28,8 +29,6 @@ function createBreedsArray(json) {
   renderBreeds(allBreeds);
 }
 
-
-
 function renderBreeds(breeds) {
   console.log("renderBreeds. Woof!!");
 
@@ -39,7 +38,11 @@ function renderBreeds(breeds) {
     let li = document.createElement("li");
     li.textContent = breed;
     dogBreedsContainer.appendChild(li);
+    li.addEventListener("click", changeColor);
   })
+
+  const breedDropdown = document.getElementById("breed-dropdown");
+  breedDropdown.addEventListener("change", sortBreeds);
 };
 
 
@@ -57,6 +60,47 @@ function printImg(json) {
     img.src = image;
     dogImageContainer.appendChild(img);
   }};
+
+  function changeColor(e) {
+    e.target.style.color = "salmon";
+  }
+
+function updateBreeds(breeds) {
+  document.getElementById("dog-breeds").innerHTML = "";
+  renderBreeds(breeds);
+}
+
+function sortBreeds(e) {
+  // console.log(e.target.value);
+  let valueToSort = e.target.value;
+
+  // For testing
+  // let string = "Hello"
+  // // breeds.startsWith(valueToSort)
+  //
+  // if (string.startsWith(valueToSort)) {
+  //   console.log("Woooooofff!")
+  // } else {
+  //   console.log("Meoooow!")
+  // }
+
+
+  updateBreeds(allBreeds.filter(breed => breed.startsWith(valueToSort)));
+
+  // Doesn't work
+  // allBreeds.forEach(breed => {
+  //   // console.log(x[0])
+  //
+  //   if (breed.startsWith(valueToSort) == breed[0]) {
+  //     console.log("Woooooofff!");
+  //   } else {
+  //     console.log(valueToSort);
+  //     console.log(breed[0]);
+  //     console.log("Meoooow!");
+  //   }
+  // })
+
+}
 
 //
 // let breeds = [];
