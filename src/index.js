d4d3a02bd161ada265e9105ce59a782c70fc5d31
Brittany Javalera <brittjavalera@gmail.com>
2020-01-17 13:20:15 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -48,15 +48,16 @@ let breedNames = []
     const alphaDropdown = document.getElementById('breed-dropdown')
     alphaDropdown.addEventListener('change', function(event) {
         const letter = event.target.value
-
+        
         let breeds = dogBreedUl.getElementsByTagName("li");
             for (let i = 0; i < breeds.length; i++) {
              breedNames.push(breeds[i].innerText)
             };
 
         let filterNames = breedNames.filter(breed => breed.startsWith(letter))
+        dogBreedUl.innerHTML = ""
         filterNames.forEach(function(name){
-            dogBreedUl.innerHTML = `<li>${name}</li>`
+            dogBreedUl.innerHTML += `<li>${name}</li>`
         });
             ///for (let i = 0; i < filterNames.length; i++) {
                 //`<li>${filterNames[i]}</li>`
