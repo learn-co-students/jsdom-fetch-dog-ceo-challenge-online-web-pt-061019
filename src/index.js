af2e7ffe31ac02b01a0053e946d4db14865597a0
Khris Punzalan <khrispunzalan@gmail.com>
2020-01-19 07:35:04 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const breedUrl = 'https://dog.ceo/api/breeds/list/all'
 document.addEventListener('DOMContentLoaded', function() { 
     fetchDogs();
     fetchBreeds();
-    filterBreeds();
+    // filterBreeds(); ->no parameters; use filter()
 })
 
 // Challenge 1
@@ -32,9 +32,10 @@ function fetchBreeds() {
     fetch(breedUrl)
     .then(response => response.json())
     .then(json => (breedArray = Object.keys(json.message)))
+    
             // Object.keys(), the return value is an Array containing all of the keys at the top level of the Object
     .then(() => showBreeds(breedArray));
-    // debugger
+ 
 };
 
 function showBreeds(breedList){
@@ -51,34 +52,42 @@ function showBreeds(breedList){
             this.style.color = 'red'
                 // this: A property of an execution context (global, function or eval) that, in non–strict mode, is always a reference to an object and in strict mode can be any value.
         });
-    });    
+    });
+    filterBreeds(breedList); //using filter()
+
 };
 // Challenge 4
-function filterBreeds() {
-    const dropdown = document.querySelector("select#breed-dropdown");
-    dropdown.addEventListener('change', function(e) {
-        const startLetter = e.target.value;
-        const liBreed = document.querySelectorAll('li');
-        liBreed.forEach(breed => {
-            if (breed.innerText.startsWith(startLetter)) {
-                breed.hidden = false
-            } else {
-                breed.hidden = true
-            };
-        });
-    });
-};
-
-
-// use filter()
 // function filterBreeds() {
 //     const dropdown = document.querySelector("select#breed-dropdown");
 //     dropdown.addEventListener('change', function(e) {
 //         const startLetter = e.target.value;
-//         let liBreed = document.querySelectorAll('li');
+//         const liBreed = document.querySelectorAll('li');
 //         liBreed.forEach(breed => {
-
+//             if (breed.innerText.startsWith(startLetter)) {
+//                 breed.hidden = false
+//             } else {
+//                 breed.hidden = true
 //             };
 //         });
 //     });
 // };
+
+// use filter() for Challenge 4
+function filterBreeds(breedList) {
+    const dropdown = document.querySelector("select#breed-dropdown");
+    dropdown.addEventListener('change', function(e) {
+        const startLetter = e.target.value;
+        const liBreed = document.querySelectorAll('li')
+        let filterLiBreed = breedList.filter((b) => {
+            return b.startsWith(startLetter)    
+        });
+        liBreed.forEach(breed => {
+            if (filterLiBreed.includes(breed.innerText)) {
+                breed.hidden = false
+            } else {
+                breed.hidden = true
+            };          
+        });
+    });
+};
+
